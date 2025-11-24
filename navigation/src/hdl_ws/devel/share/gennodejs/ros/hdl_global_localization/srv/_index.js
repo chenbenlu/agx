149c0d8408d460372892d72d2d14992da4c28c64
nycusystemlab <jw30414258@gmail.com>
2025-11-24 22:50:01 +0800

    hdl & lidar workspace

---
+++ navigation/src/hdl_ws/devel/share/gennodejs/ros/hdl_global_localization/srv/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let QueryGlobalLocalization = require('./QueryGlobalLocalization.js')
+let SetGlobalMap = require('./SetGlobalMap.js')
+let SetGlobalLocalizationEngine = require('./SetGlobalLocalizationEngine.js')
+
+module.exports = {
+  QueryGlobalLocalization: QueryGlobalLocalization,
+  SetGlobalMap: SetGlobalMap,
+  SetGlobalLocalizationEngine: SetGlobalLocalizationEngine,
+};
---
+++ navigation/src/lidar_ws/build/LeGO-LOAM/cloud_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/systemlabagx/lidar_ws/src/LeGO-LOAM/cloud_msgs
+# Build directory: /home/systemlabagx/lidar_ws/build/LeGO-LOAM/cloud_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
